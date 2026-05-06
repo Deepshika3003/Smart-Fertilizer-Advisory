d846fdbe9ce4b65536abba25b7330089a4010371
Deepshika <deepshikapagidimarri47@gmail.com>
2026-05-06 10:13:28 +0530

    add test route

--- backend/app.py
+++ backend/app.py
@@ -130,5 +130,8 @@ def test():
 # Run App
 # ============================================
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
